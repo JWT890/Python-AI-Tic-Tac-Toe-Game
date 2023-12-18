b3a01057c93b0266994af96f444291089ccf715a
Jon William Taylor <jwtaylor659@gmail.com>
2023-12-18 16:08:07 -0600

    first commit

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or \
+           all(board[i][j] == player for j in range(3)):
+               return True
+        
+        if all(board[i][j] == player for j in range(3)) or \
+           all(board[i][2 - i] == player for j in range(3)):
+             return True
+        
+        return False
+    
